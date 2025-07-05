6ee90cc02c1fa34fda3fb83f4886fdb8523a3200
Devraj Singh Kurmi <devrajsinghkurmi2003@gmail.com>
2025-07-05 16:21:16 +0530

    Fix: update server.js for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,10 @@ app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
+app.get('/',(req,res)=>{
+    res.send("api working")
+})
+
 
 app.listen(port,()=>{
     console.log(`Server started on PORT: ${port}`);
